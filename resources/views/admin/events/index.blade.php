75ddf046f7d1f190a7a2c19d782c69b2a4a3eded
supe3r00t <ksa0.net@email.com>
2023-03-26 01:34:33 +0300

    added some new

--- resources/views/admin/events/index.blade.php
+++ resources/views/admin/events/index.blade.php
@@ -14,7 +14,7 @@
     <div class="card" style="width: 10rem;">
         <div class="card-body">
             <h5 class="card-title">انشاء احداث</h5>
-            <a href="{{route('events.events')}}" class="btn btn-secondary stretched-link">Create</a>
+            <a href="{{route('events.create')}}" class="btn btn-secondary stretched-link">Create</a>
         </div>
     </div>
 
